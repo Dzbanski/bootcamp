72e210e5a1a62dcd7ad78f356b5a733a76a68c1f
Jacek Dzbański <jacek17123@gmail.com>
2026-01-12 00:44:44 +0100

    Add function is_palindrom

--- palindrom.py
+++ palindrom.py
@@ -1,1 +1,10 @@
-print("To")
+
+def is_palindrom(name):
+    name_low = name.lower()
+    
+    return name_low == name_low[::-1]
+
+check_palindrom = is_palindrom("Kajak")
+
+print(check_palindrom)
+
